4c97d2b3afb7025cf038248bb70fff22e5fbe996
phamdinhbinh <phamdinhbinhonly1@gmail.com>
2023-01-31 14:32:36 +0700

    fix surge

--- script.js
+++ script.js
@@ -7,9 +7,7 @@ function calculate(){
     var SoTienTraHangThang = ((SoTienVay*x*LaiSuatTheoThang)/(x-1));
     var TongSoTienPhaiTra = SoTienTraHangThang*SoThang;
     var TongSoLaiPhaiTra  = TongSoTienPhaiTra-SoTienVay;
-    // var SoTien1Thang = (SoTienVay/12).toFixed(1);
-    // var TongTien = SoThang * 30;
-    // var X = (Math.pow(1+LaiSuatTheoThang, SoThang));
+    
     var result1 = document.getElementById("SoTienTraHangThang");
     var result2 = document.getElementById("TongSoTienPhaiTra");
     var result3 = document.getElementById("TongSoLaiPhaiTra");
